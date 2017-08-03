809e371b2a251292450045e807e79910705d5eac
NajibAdan <najibadan1@gmail.com>
2017-08-03 13:45:17 +0300

    Search function
    It now can search for the manga instead of receiving the manga url

--- manga.py
+++ manga.py
@@ -9,10 +9,13 @@ import os
 import time
 
 chapter_links = []
+scraper = cfscrape.create_scraper()
+#ensures the chapter directory exists, if not it creates a new one
 def ensure_dir(filepath):
 	directory = os.path.dirname(filepath)
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+#downloads the pages
 def download(url_link,filepath):
 	try:
 		with open(filepath,'wb') as output:
@@ -21,13 +24,31 @@ def download(url_link,filepath):
 		#whenever there is a disconnection
 		time.sleep(5)
 		download(url_link,filepath)
+#searches for the manga
+def search(search_term):
+	linksearchobj =[]
+	searchurl = 'http://kissmanga.com/Search/SearchSuggest'
+	data = {
+		'type':'manga',
+		'keyword': search_term
+	}
+	html = connection(searchurl,True,data)
+	soup = BeautifulSoup(html)
+	print 'Mangas found: '
+	for i in soup.findAll('a'):
+		linksearchobj.append(i['href'])
+		print str(len(linksearchobj)) + ': ' + i.text
+	if len(linksearchobj) > 1:
+		choice = input('Enter your choice: ')
+		links(linksearchobj[choice-1])
+	else:
+		links(linksearchobj[0])
 
-def chapters(base_url,manga_name):
-	chapters = len(chapter_links)
+def chapters(manga_name):
 	for i in chapter_links:
-		chapters = i[i.rfind('/')+1:i.rfind('?id')]
-		filepath = manga_name +'\\'+chapters + '\\'
-		url = base_url+i
+		chapter = i[i.rfind('/')+1:i.rfind('?id')]
+		filepath = manga_name +'\\'+chapter + '\\'
+		url = 'http://kissmanga.com'+i
 		print url
 		html = connection(url)
 		soup = BeautifulSoup(html)
@@ -42,15 +63,19 @@ def chapters(base_url,manga_name):
 			if (not os.path.isfile(filepath+str(page_counter)+extension)):
 				download(result,filepath+str(page_counter)+extension)
 
-def connection(url,seconds = 5):
+def connection(url,Post = False,data=None,seconds = 5):
 	try:
-		soup = scraper.get(url).content
-		return soup
+		if Post is False:
+			soup = scraper.get(url).content
+			return soup
+		else:
+			soup =  scraper.post(url,data).content
+			return soup
 	except:
 		print 'No connection, retrying in '+str(seconds)+' seconds'
 		time.sleep(seconds)
 		connection(url,seconds)
-
+#provides the link to the manga pages
 def new_decoder(url):
 	#from https://gist.github.com/nhanb/74542c36d3dcc5dde4e90b34437fb523
 	iv = 'a5e8e2e9c2721be0a84ad660c472c1f3'.decode('hex')
@@ -63,18 +88,17 @@ def new_decoder(url):
 	# unpad
 	result = result[:-ord(result[-1])]
 	return result
+#gets the links to the manga chapters
+def links(thisurl):
+	soup = BeautifulSoup(connection(thisurl))
+	print thisurl
+	manga_name = soup.find('link', {'rel': 'alternate'})['title'][:-6]
+	_manga_name = thisurl[27:]
+	print manga_name
+	for i in soup.findAll('a',href=re.compile('/Manga/'+_manga_name)):
+		if '?' in i['href']:
+			chapter_links.append(i['href'])
+	chapters(manga_name)
 
-base_url = 'http://kissmanga.com'
-scraper = cfscrape.create_scraper()
-thisurl = raw_input('Enter the manga link: ')
-
-#Feed HTML file into parser
-soup = BeautifulSoup(connection(thisurl))
-manga_name = soup.find('link', {'rel': 'alternate'})['title'][:-6]
-_manga_name = thisurl[27:]
-print _manga_name
-for item in soup.findAll('a',href=re.compile('/Manga/'+_manga_name)):
-	if '?' in item['href']:
-		chapter_links.append(item['href'])
-
-chapters(base_url,manga_name)
+searchterm = raw_input('Manga name: ')
+search(searchterm)
